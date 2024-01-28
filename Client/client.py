dab31fdd03efab373daad4b3445d08a6b93f113f
Stanical <arsamezzati99@gmail.com>
2024-01-28 16:01:36 +0100

    project completed

--- Client/client.py
+++ Client/client.py
@@ -1,22 +1,17 @@
 import streamlit as st
 import requests
 
-
 st.title('Send Request to Server')
 
+# initializing the session if not exists already
+if 'conversation_history' not in st.session_state:
+    st.session_state.conversation_history = []
 
-# user input text-field
-user_input = st.text_area("Enter your request:")
-
-
-if st.button('Submit'):
-    # creating the json file after submit button is clicked
+if prompt := st.chat_input("Ask your question, press Enter to submit"):
     request_data = {
         "contents": {
             "role": "user",
-            "parts": {
-                "text": user_input
-            }
+            "parts": prompt
         },
         "safety_settings": {
             "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
@@ -28,24 +23,29 @@ if st.button('Submit'):
             "maxOutputTokens": 2048
         }
     }
-
-    # for displaying the actual json file
-    # st.json(request_data)
-
-    # sending the json to the server
     url = 'http://localhost:8000/send_request'
     response = requests.post(url, json=request_data)
 
-    # displaying the response
+    # checking http response status code
     if response.status_code == 200:
-        st.success("Results: ")
         response_data = response.json()
-        for item in response_data:
-            for candidate in item.get("candidates", []):
-                content = candidate.get("content", {})
-                text_parts = content.get("parts", [])
-                for part in text_parts:
-                    text = part.get("text", "")
-                    st.write(text)
+        # saving the response to history
+        st.session_state.conversation_history.append((prompt, response_data))
+
+        # displaying old and new Q/A by iterating through the key value pair
+        for prompt, response in st.session_state.conversation_history:
+
+            with st.chat_message("user"):
+                st.write(prompt)
+            with st.chat_message("assistant"):
+                for item in response:
+                    for candidate in item.get("candidates", []):
+                        content = candidate.get("content", {})
+                        text_parts = content.get("parts", [])
+
+                        for part in text_parts:
+                            text = part.get("text", "")
+                            st.write(text)
+
     else:
         st.error(f"Failed to get response. Status code: {response.status_code}")

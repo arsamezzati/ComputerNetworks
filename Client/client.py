1e4e8aa750f53c3f7ffd77efec32c887cec94d6d
Stanical <arsamezzati99@gmail.com>
2024-01-28 16:28:24 +0100

    project completed

--- Client/client.py
+++ Client/client.py
@@ -3,7 +3,7 @@ import requests
 
 st.title('Send Request to Server')
 
-# Initialize session state for conversation history if not already present
+# initializing the session
 if 'conversation_history' not in st.session_state:
     st.session_state.conversation_history = []
 
@@ -11,7 +11,7 @@ if prompt := st.chat_input("Ask your question, press Enter to submit"):
     request_data = {
         "contents": {
             "role": "user",
-            "parts": [{"text": prompt}]  # parts should be a list of dictionaries
+            "parts": [{"text": prompt}]
         },
         "safety_settings": {
             "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
@@ -26,19 +26,19 @@ if prompt := st.chat_input("Ask your question, press Enter to submit"):
     url = 'http://localhost:8000/send_request'
     response = requests.post(url, json=request_data)
 
-    # Displaying the response
+    # checking the status code of the http response
     if response.status_code == 200:
         response_data = response.json()
-        # Store user question and server response in conversation history
+        # keeping state
         st.session_state.conversation_history.append((prompt, response_data))
 
-        # Display conversation history
+        # displaying old and new Q/A
         for prompt, response in st.session_state.conversation_history:
 
             with st.chat_message("user"):
                 st.write(prompt)
             with st.chat_message("assistant"):
-                for item in response:  # Assuming response is a list of items
+                for item in response:
                     for candidate in item.get("candidates", []):
                         content = candidate.get("content", {})
                         text_parts = content.get("parts", [])
